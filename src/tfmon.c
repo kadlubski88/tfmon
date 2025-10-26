aa236a0c3bc49c496731ad6b9f376ff4dfe61f53
Georges Kadlubski <georgeskadlubski@gmail.com>
2025-10-26 17:32:29 +0100

    create first structure

---
+++ src/tfmon.c
@@ -0,0 +1,13 @@
+//###########################################################################
+//# Text File MONitor(tfmon): watch a single or multiple small text         # 
+//# files in the command line and follow changes.                           #
+//# https://github.com/kadlubski88/tfmon                                    #
+//#                                                                         #
+//# The MIT License (MIT)                                                   #
+//# Copyright © 2025 Georges Kadlubski                                      #
+//# URL: https://mit-license.org/                                           #
+//###########################################################################
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
